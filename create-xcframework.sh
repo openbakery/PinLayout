950025f04b8a749867cf334675a7c45f37e436dd
René Pirringer <rene@pirringer.at>
2025-05-09 11:46:26 +0200

    updated xcframework script to be more generic

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -9,32 +9,47 @@ then
 fi
 
 VERSION=$1
+NAME=PinLayout
+PROJECT=$NAME.xcodeproj
 
-xcodebuild archive \
-    -project PinLayout.xcodeproj \
-    -scheme PinLayout \
+carthage bootstrap --platform iOS --use-xcframeworks --cache-builds
+
+build() {
+	local FRAMEWORK_NAME=$1
+
+	echo "Building $FRAMEWORK_NAME"
+
+	xcodebuild archive \
+    -project $PROJECT \
+    -scheme $FRAMEWORK_NAME \
     -destination "generic/platform=iOS" \
-    -archivePath "$ARCHIVE_DIRECTORY/PinLayout-iOS" \
+    -archivePath "$ARCHIVE_DIRECTORY/$FRAMEWORK_NAME-iOS" \
     SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcbeautify --disable-colored-output
 
-xcodebuild archive \
-    -project PinLayout.xcodeproj \
-    -scheme PinLayout \
+	xcodebuild archive \
+    -project $PROJECT \
+    -scheme $FRAMEWORK_NAME \
     -destination "generic/platform=iOS Simulator" \
-    -archivePath "$ARCHIVE_DIRECTORY/PinLayout-iOS_Simulator" \
+    -archivePath "$ARCHIVE_DIRECTORY/$FRAMEWORK_NAME-iOS_Simulator" \
     SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcbeautify --disable-colored-output
 
 
-xcodebuild -create-xcframework \
-    -archive $ARCHIVE_DIRECTORY/PinLayout-iOS.xcarchive -framework PinLayout.framework \
-    -archive $ARCHIVE_DIRECTORY/PinLayout-iOS_Simulator.xcarchive -framework PinLayout.framework \
-    -output build/xcframework/PinLayout.xcframework
+	xcodebuild -create-xcframework \
+		-archive $ARCHIVE_DIRECTORY/${FRAMEWORK_NAME}-iOS.xcarchive -framework ${FRAMEWORK_NAME}.framework \
+		-archive $ARCHIVE_DIRECTORY/${FRAMEWORK_NAME}-iOS_Simulator.xcarchive -framework ${FRAMEWORK_NAME}.framework \
+		-output $ARCHIVE_DIRECTORY/${FRAMEWORK_NAME}.xcframework
+}
+
+build $NAME
 
 mkdir build/xcframework
 
-cp LICENSE build/xcframework
-cd build/xcframework
 
-zip -r ../xcframework/PinLayout-$VERSION.zip PinLayout.xcframework LICENSE
+cp LICENSE $ARCHIVE_DIRECTORY
+mkdir build/xcframework
+
+cd $ARCHIVE_DIRECTORY
+
+zip -r ../xcframework/$NAME-$VERSION.zip $NAME.xcframework LICENSE
