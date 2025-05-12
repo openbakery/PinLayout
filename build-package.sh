97e1366f8cc970c19e324856e2f4dfdc7dbe0e3b
René Pirringer <rene@pirringer.at>
2025-05-12 10:54:40 +0200

    added xcframework to zip name

--- build-package.sh
+++ build-package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swift build -Xswiftc "-sdk" -Xswiftc `xcrun --sdk iphonesimulator --show-sdk-path` -Xswiftc "-target" -Xswiftc "arm64-apple-ios16.2-simulator"
+swift build -Xswiftc "-sdk" -Xswiftc `xcrun --sdk iphonesimulator --show-sdk-path` -Xswiftc "-target" -Xswiftc "arm64-apple-ios18.4-simulator"
